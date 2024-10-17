e249117737338abb22c57885c096cf91e8ac90ac
aksenmi <aksendev@gmail.com>
2024-10-17 11:02:10 +0900

    환경설정 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true, // Strict Mode 활성화
+
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/logIn",
+        permanent: false, // 영구 리다이렉트 여부 (개발 단계에서는 false 권장)
+      },
+    ];
+  },
 };
 
 export default nextConfig;
